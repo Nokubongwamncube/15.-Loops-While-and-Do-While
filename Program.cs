bac2b73b6a1d614148a5b91e9e2d6c379eb8d6fd
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-14 10:25:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Loops__While_and_Do_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is now the normal while loop");
+            int  index = 6;
+
+            while (index <= 5) {
+                Console.WriteLine(index);
+                index++;
+
+
+            }
+
+            Console.WriteLine("This is now the DO While loop");
+            int index2 = 6;
+            do
+            {
+
+                Console.Write(index2);
+                index2++;
+
+            }
+            while (index2 <= 5);
+
+            //freeze 
+            Console.Read();
+
+
+              
+
+
+
+        }
+    }
+}
